88741028993f4250dcc8cdec667b095e9320d45b
std-022-024 <romntix74@yandex.ru>
2023-11-13 22:40:02 +0300

    external IP added

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 provider "yandex" {
-  #token = "ajeo76etaclov4mc2sfr"
   cloud_id  = "b1g3jddf4nv5e9okle7p"
   folder_id = "b1gm68de4u794bg8egku"
   zone      = "ru-central1-a"
@@ -37,14 +36,15 @@ resource "yandex_compute_instance" "vm-1" {
     # нужно указать идентификатор подсети, к которой будет подключена ВМ
     network_interface {
         subnet_id = "e9bmq2f27li3v3u21i30"
-        # nat       = false
+        nat       = true
     }
 
     # Метаданные машины:
     # здесь можно указать скрипт, который запустится при создании ВМ
     # или список SSH-ключей для доступа на ВМ
     metadata = {
-        ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+        #ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+        user-data = "${file("meta.txt")}"
     }
     
 }
